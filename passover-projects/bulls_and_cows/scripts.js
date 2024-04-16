db27c036baa9fe61ea42bed2e06f92ec3d743d31
EdenR7 <edenrot777@gmail.com>
2024-04-16 20:59:15 +0300

    bulls&cows-update

--- passover-projects/bulls_and_cows/scripts.js
+++ passover-projects/bulls_and_cows/scripts.js
@@ -1,9 +1,20 @@
 let username;
 let boardsize;
-const targetBoard = [];
+let targetBoard = [];
 let userBoard = [];
 let guessesContainerElement = document.querySelector(".guesses-container");
 let curIndexToReplace = 0;
+let tries = 0;
+let clock = {
+    seconds : 0,
+    minutes : 0,
+    hours : 0, 
+    displayTimer : function(){
+        return(`${this.hours.toString().length == 1 ? `0${this.hours}`:this.hours}:${this.minutes.toString().length == 1 ? `0${this.minutes}`:this.minutes}:${this.seconds.toString().length == 1 ? `0${this.seconds}`:this.seconds}`);
+    }
+};
+let timeInterval;
+let isTimerRun = false;
 
 
 function startGame(){
@@ -18,6 +29,9 @@ function startGame(){
         generateRandomBoard(boardsize);
         generateFirstCard();
         generateNewUserTurn();
+        document.querySelector(".greet-message").innerText = `Hello ${username} welcome to Bulls&Cows !`;
+        document.querySelector("#mistakes").innerText = tries;
+        startStopTimer();
     }
 }
 function generateRandomBoard(size){
@@ -30,14 +44,34 @@ function generateRandomBoard(size){
         options.splice(randomIndex, 1);
     }
 }
+function startStopTimer(){
+    if(! isTimerRun){
+        timeInterval = window.setInterval(function(){
+            clock.seconds ++;
+            if(clock.seconds == 60){
+                clock.seconds = 0;
+                clock.minutes ++;
+            }
+            if(clock.minutes == 60){
+                clock.minutes = 0;
+                clock.hours ++;
+            }
+            document.querySelector("#stopwatch").innerText = clock.displayTimer();
+        }, 1000);
+        isTimerRun = true;
+    } else{
+        window.clearInterval(timeInterval);
+        isTimerRun = false;
+    }
+}
 
 function generateFirstCard(){
     let firstCard = document.querySelector("#title-card");
-    targetBoard.forEach((number) => {
+    for (let index = 0; index < targetBoard.length; index++) {
         firstCard.innerHTML += `<div class = "number-div"> ${"?"}</div>`;
-    });
-    firstCard.innerHTML += `<div class = "bulls">Bulls</div>`;
-    firstCard.innerHTML += `<div class = "cows">Cows</div>`;
+    }
+    firstCard.innerHTML += `<div id = "first-bull">Bulls</div>`;
+    firstCard.innerHTML += `<div id = "first-cow">Cows</div>`;
 }
 function generateNewUserTurn(){
     let innerHtmlToAdd = `<div class="guess-card flex-group">`;
@@ -63,11 +97,9 @@ function checkTurn(){
     if(!isValidTurn()){
         alert("You have duplicates in your try !")
     }else{
-        const bulls = checkCowsAndBulls().bulls, cows = checkCowsAndBulls().cows; 
-        // delete last guess card and transfer to div card instead of buttons
+        const bulls = checkCowsAndBulls().bulls, cows = checkCowsAndBulls().cows; // delete last guess card and transfer to div card instead of buttons
         removeLastChild()
-        // transfer the last row to div rows instead of buttons
-        let innerHtmlToAdd = `<div class="guess-card flex-group">`;
+        let innerHtmlToAdd = `<div class="guess-card flex-group">`;// transfer the last row to div rows instead of buttons
         userBoard.forEach((number) => {
             innerHtmlToAdd += `<div class = "number-div"> ${number}</div>`;
         });
@@ -75,12 +107,15 @@ function checkTurn(){
         innerHtmlToAdd += `<div class = "cows">${cows}</div>`;
         innerHtmlToAdd += `</div>`;
         guessesContainerElement.innerHTML += innerHtmlToAdd;
-        // check game over 
-        if(bulls == targetBoard.length){
-            alert("Done!")
+        document.querySelector("#mistakes").innerText = tries;
+        if(bulls == targetBoard.length){// check game over 
+            startStopTimer();
+            gameOver();
         } else{
             generateNewUserTurn()
-        }}
+        }
+        tries ++;
+    }
 }
 function isValidTurn(){
     let setToCheck = new Set(userBoard);
@@ -105,4 +140,25 @@ function checkCowsAndBulls() {
     return { bulls, cows };
 }
 
+function gameOver (){
+    document.querySelector("#overlay-message").style.display = "flex";
+    document.querySelector("#title-overlay").innerText = `Well done ${username} the numbers were ${targetBoard}`;
+    document.querySelector("#time-overlay").innerText = `Time to solve: ${clock.displayTimer()}`;
+    clock.hours = 0, clock.minutes = 0, clock.seconds = 0;
+    document.querySelector("#tries-overlay").innerText = `${tries} tries`;
+    tries = 0;
+}
+function closeOverlay(){
+    document.querySelector("#overlay-message").style.display = "none";
+}
+
+function playAgain(){
+    document.querySelector("#overlay-message").style.display = "none";
+    guessesContainerElement.innerHTML = '<div class="guess-card flex-group" id="title-card"></div>';
+    targetBoard = [];
+    userBoard = [];
+    curIndexToReplace = 0;
+    tries = 0;
+    startGame();
+}
 
