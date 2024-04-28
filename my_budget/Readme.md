29c6b013b4b173fd24d010782b853b876ce31dc1
EdenR7 <edenrot777@gmail.com>
2024-04-28 13:44:23 +0300

    myBudget-done

--- my_budget/Readme.md
+++ my_budget/Readme.md
@@ -1,15 +1,20 @@
-Data structure 
-- ONE OBJECT contains object incomes and object expenses. 
+Data structure
+
+- ONE OBJECT contains object incomes and object expenses.
   Each of the objects contains array of objects(contain value of the expense and the description) and total amount.
 
+Operations
 
-Operations 
 - display the date
 - new data validations
 - add data to its array and display it
 - calculate the % of each outcome and display it
-- add the edit data option(delete or changes)
+- remove action from the data
 
+features
 
-flow 
-- 
+- Date is updating
+- different colors of the form according to the current action
+- every second element will always colored a bit darker (changing according to its index)
+- remove elements from the data
+- the precentages will be updating after every action from the user (unless the expenses will be greater than the incomes)
--- my_budget/scripts.js
+++ my_budget/scripts.js
@@ -12,15 +12,15 @@ let totalBudget = { // Main data structure
     }
 };
 
-
 const date = getDate();
 document.getElementById("date").innerText = date;
 function getDate(){
     const curDate = new Date();
     return `${curDate.toLocaleString('en-US', { month: 'long' })} ${curDate.getFullYear()}`;
 }
+document.getElementById("add-description").focus();
 
-
+//validations
 function validateDescription(){
     const element = document.getElementById("add-description");
     if (element.value == ""){
@@ -44,12 +44,30 @@ function validateValueInput(){
     }
 }
 
-function addNewAction(typeOfAction, description, amount){ // display the new action on the screen seperate between income to expense
+// adding new elements functions
+function addMouseEvents(elementID, isIncome) {
+    let toAddFlag, toRemoveFlag;
+    // in case of expenses the value wrapper conatin also the precentage so it has one more child
+    isIncome ? (toAddFlag = 1, toRemoveFlag = 2) : (toAddFlag = 2, toRemoveFlag = 3); 
+    const element = document.getElementById(elementID);
+    const elementTargetDiv = element.children[element.children.length-1]; // The div wrapper of the new element 
+    element.addEventListener('mouseover', function() {
+        if (elementTargetDiv.children.length === toAddFlag) {
+            elementTargetDiv.innerHTML += `<button id="cancel-btn" onclick = "removeElement(event)"></button>`;
+        }
+    });
+    element.addEventListener('mouseleave', function() { // remove the btn when the mouse left the container
+        if (elementTargetDiv.children.length === toRemoveFlag) {
+            elementTargetDiv.removeChild(elementTargetDiv.lastChild);
+        }
+    });
+}
+function addNewActionToScreen(typeOfAction, description, amount){ // display the new action on the screen seperate between income to expense
     if (typeOfAction === "income"){
         let newElementHtml = `
         <div id="income-${totalBudget.incomes.descriptions.length}" class="flex-group data-card">
             <div class="description-text">${description}</div>
-            <div class="value-text">+ ${amount.toFixed(2)}</div>
+            <div class="value-text flex-group"><p>+ ${amount.toFixed(2)}</p></div>
         </div>`;
         document.getElementById("income").innerHTML += newElementHtml;
     } else if(typeOfAction === "expense"){
@@ -64,7 +82,7 @@ function addNewAction(typeOfAction, description, amount){ // display the new act
         document.getElementById("expenses").innerHTML += newElementHtml;
     }
 }
-function updateChanges(){ // update the main object and also call the addNewAction to display the new action on the screen
+function updateChanges(){ // update the main object and also call the addNewActionToScreen to display the new action on the screen
     const actionDescription = document.getElementById("add-description").value;
     const actionValue = Number(document.getElementById("value").value);
     if (document.getElementById('symbol-type').value === "+") { // income
@@ -73,17 +91,17 @@ function updateChanges(){ // update the main object and also call the addNewActi
             description: actionDescription,
             amount: actionValue
         });
-        addNewAction("income", actionDescription, actionValue);
+        addNewActionToScreen("income", actionDescription, actionValue);
     }else if (document.getElementById('symbol-type').value === "-") { // expense
         totalBudget.expenses.total += actionValue;
         totalBudget.expenses.descriptions.push({
             description: actionDescription,
             amount: actionValue
         });
-        addNewAction("expense", actionDescription, actionValue);
+        addNewActionToScreen("expense", actionDescription, actionValue);
     }
 }
-function precentageUpdate(){
+function precentageUpdate(){ // update the precentage of the expenses of the total incomes
     const totalBudgetElemnent = document.getElementById("total-precentage");
     const displayPrecentage = totalBudget.expenses.total <= totalBudget.incomes.total;
     if (displayPrecentage && totalBudget.expenses.total > 0) {
@@ -91,6 +109,7 @@ function precentageUpdate(){
     } else{
         totalBudgetElemnent.innerText = "-%";
     }
+    // the precentages all the expenses elements
     const expensesPrecentageElements = document.querySelectorAll(".new-expense-precentage")
     totalBudget.expenses.descriptions.forEach((element, index) => {
         if (displayPrecentage) {
@@ -101,31 +120,32 @@ function precentageUpdate(){
     });
     
 }
-function dispalyTotalBudgetUpdate(){
+function dispalyTotalBudgetUpdate(){ 
+    // updating the total incomes and the total expenses
     document.getElementById("current-incomes").innerHTML=`+ ${totalBudget.incomes.total.toFixed(2)}`;
     document.getElementById("current-expenses").innerHTML= `- ${Math.abs(totalBudget.expenses.total).toFixed(2)}`;
-    if (totalBudget.getTotalCalculation() > 0) { 
+     // update the total budget
+    if (totalBudget.getTotalCalculation() >= 0) {
         document.getElementById("total-budget").innerHTML = `+ ${totalBudget.getTotalCalculation().toFixed(2)}`;
     }else{
         document.getElementById("total-budget").innerHTML = `- ${Math.abs(totalBudget.getTotalCalculation()).toFixed(2)}`;
     }
 }
-function matchIdToIndex() { // make sure the id's of each element still match its index in case of change, also colored the bgc of each second element
-
-    // both include the title h3 of each div at index 0
+function matchIdToIndex() { // important function to keep the input/expense elements in the right order 
+    
     const incomeElements = Array.from(document.querySelector("#income").children);
     const expensesElements = Array.from(document.querySelector("#expenses").children);
-    incomeElements.forEach((element, index) => {
-        index === 0 ? element.id = `incomes-title` : element.id = `income-${index}`; 
-        if(index % 2 === 0 && index > 0){
-            element.style.backgroundColor = "hsl(0, 0%, 97%)"
-        }
+
+    // iterate over all the expenses/incomes elements, make sure that they id's match their indexes, change every second element bgc
+    incomeElements.slice(1).forEach((element, index) => {
+        element.id = `income-${index+1}`; // include the title h3 of each div at index 0
+        index % 2 === 1 ? element.style.backgroundColor = "hsl(0, 0%, 97%)": element.style.backgroundColor = "";
+        addMouseEvents(element.id, isIncome = true); 
     });
-    expensesElements.forEach((element, index) => {
-        index === 0 ? element.id = `expenses-title` : element.id = `expense-${index}`; 
-        if(index % 2 === 0 && index > 0){
-            element.style.backgroundColor = "hsl(0, 0%, 97%)"
-        }
+    expensesElements.slice(1).forEach((element, index) => {
+        element.id = `expense-${index+1}`; // include the title h3 of each div at index 0
+        index % 2 === 1 ? element.style.backgroundColor = "hsl(0, 0%, 97%)": element.style.backgroundColor = "";
+        addMouseEvents(element.id, isIncome = false);
     });
 }
 function submitInput(){ // new action by the user
@@ -136,8 +156,10 @@ function submitInput(){ // new action by the user
         matchIdToIndex();
         document.getElementById("add-description").value = "";
         document.getElementById("value").value = "";
+        document.getElementById("add-description").focus();
     }
 }
+// adding the options to press enter to activate the button
 document.getElementById("value").addEventListener("keydown", function(event){ // adding the option to submit by presseing enter
     if (event.key === 'Enter') {
         submitInput();
@@ -148,6 +170,36 @@ document.getElementById("add-description").addEventListener("keydown", function(
         submitInput();
     }
 });
+
+// removing elements and updating functions
+function removeElementFromObject(ancestorID, elementIndex) {
+    elementIndex --; // because the title is also a child, the count of the element indexes in the dom is from 1 and not 0;
+    let elementValue;
+    if(ancestorID == 'income'){
+        elementValue = totalBudget.incomes.descriptions[elementIndex].amount;
+        totalBudget.incomes.descriptions.splice(elementIndex, 1);
+        totalBudget.incomes.total -= elementValue;
+    } else{
+        elementValue = totalBudget.expenses.descriptions[elementIndex].amount;
+        totalBudget.expenses.descriptions.splice(elementIndex, 1);
+        totalBudget.expenses.total -= elementValue;
+    }
+}
+function removeElement(event){
+    // get access to the desired ancestors
+    const elementToRemove = event.target.parentNode.parentNode;
+    const elementToRemoveAncestor = elementToRemove.parentNode;
+    const elementIndex = (elementToRemove.id.split('-')[1]);
+    // update the main object, remove the element;
+    removeElementFromObject(elementToRemoveAncestor.id, elementIndex)
+    // remove the element from the dom
+    elementToRemoveAncestor.removeChild(elementToRemoveAncestor.children[elementIndex]);
+    matchIdToIndex();
+    //update the values displayed on the screen (total, total incomes/expenses);
+    dispalyTotalBudgetUpdate();
+    // iterate over all the expenses (if exist) and update the precentages;
+    precentageUpdate();
+}
 
 //Change the form style in case of typing new input or new expense
 document.getElementById('symbol-type').addEventListener('change', function(){ // change the check mark and the colors of the form
@@ -164,8 +216,7 @@ function incomeInputdisplay() {
         element.classList.add("income-field");
     });
     const btnElement = document.querySelector(".img-container");
-    btnElement.innerHTML = '<img src="img/check-mark-symbol-tick.jpg" alt="green check mark" />';
-    btnElement.style.borderColor = "green";
+    btnElement.innerHTML = '<img src="img/green_checkmark.jpeg" alt="green check mark" />';
 }
 function expensesInputdisplay() {
     const elementsToChange = document.querySelectorAll(".income-field");
@@ -175,9 +226,5 @@ function expensesInputdisplay() {
     });
 
     const btnElement = document.querySelector(".img-container");
-    btnElement.innerHTML = '<img src="img/red-tick-checkmark-icon.jpg"  alt="red check mark" />';
-    btnElement.style.borderColor = "red";
+    btnElement.innerHTML = '<img src="img/red_checkmark.jpeg"  alt="red check mark" />';
 }
-
-// add the edit option
-// make it responsive 
